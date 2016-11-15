939a705d2c4981a6dfc1509a6990795d52be6cc7
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2016-11-15 17:24:21 +0100

    Houston, we have a client!

--- client/src/introsde/client/Main.java
+++ client/src/introsde/client/Main.java
@@ -3,43 +3,159 @@ package introsde.client;
 import introsde.common.to.Person;
 import org.glassfish.jersey.client.ClientConfig;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.net.URI;
-import javax.ws.rs.client.Client;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.*;
+import javax.xml.bind.JAXBException;
 
 public class Main {
 
-    private static final String ENDPOINT = "http://localhost:8080/";
+    //private static final String ENDPOINT = "http://localhost:8080/";
+    private static final String ENDPOINT = "https://introsde-a2-server.herokuapp.com/";
+
+    private static final URI SERVER_URI = UriBuilder.fromUri(ENDPOINT).build();
+    private static final WebTarget SERVER = ClientBuilder.newClient(new ClientConfig()).target(SERVER_URI);
+
+    private static final Map<String, Object> SHARED_VALUES = new HashMap<>();
 
     public static void main(String[] args) {
-        ClientConfig clientConfig = new ClientConfig();
-        Client client = ClientBuilder.newClient(clientConfig);
+        System.out.println("Server URL: " + ENDPOINT + "\n");
+        try (PrintWriter xmlOut = new PrintWriter(new FileOutputStream(new File("client-server-xml.log")));
+             PrintWriter jsonOut = new PrintWriter(new FileOutputStream(new File("client-server-json.log")))) {
+            for (int i = 1; i <= 12; i++) {
+                Method method = Main.class.getMethod("request" + i, String.class);
+                RequestLog response = (RequestLog) method.invoke(null, MediaType.APPLICATION_XML);
+                response.log(xmlOut);
+                response = (RequestLog) method.invoke(null, MediaType.APPLICATION_JSON);
+                response.log(jsonOut);
+            }
+        } catch (Exception ex) {
+            System.err.println("An error occurred while executing request.");
+            System.err.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
 
-        URI serverURI = UriBuilder.fromUri(ENDPOINT).build();
-        WebTarget service = client.target(serverURI);
+    public static RequestLog request1(String mediaType) throws JAXBException {
+        System.out.println("Executing request1 with " + mediaType);
+        String url = "person";
+        Response response = SERVER.path(url).request().accept(mediaType).get();
+        response.bufferEntity();
+        List<Person> people;
+        String body;
+        switch (mediaType) {
+            case MediaType.APPLICATION_JSON:
+                Map<String, List<Person>> peopleMap = response.readEntity(new GenericType<Map<String, List<Person>>>() {
+                });
+                people = peopleMap.get("people");
+                body = RequestLog.jsonToPrettyString(response.readEntity(String.class));
+                break;
+            case MediaType.APPLICATION_XML:
+                people = response.readEntity(new GenericType<List<Person>>() {
+                });
+                body = RequestLog.xmlToPrettyString(response.readEntity(String.class));
+                break;
+            default:
+                throw new RuntimeException("Invalid media type: " + mediaType);
+        }
 
+        String result;
+        if (people.size() >= 2) {
+            result = "OK";
+            SHARED_VALUES.put("first_person_id", people.get(0).getId());
+            SHARED_VALUES.put("last_person_id", people.get(people.size() - 1).getId());
+        } else {
+            result = "ERROR";
+        }
+        RequestLog requestLog = new RequestLog(1, HttpMethod.GET, url, mediaType, mediaType, result,
+                response.getStatus(), body);
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
 
-        System.out.println(service.path("person/8").request().accept(MediaType.APPLICATION_JSON).get().readEntity(Person.class));
+    public static RequestLog request2(String mediaType) {
+        System.out.println("Executing request2 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
 
-        /*
+    public static RequestLog request3(String mediaType) {
+        System.out.println("Executing request3 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
 
-        // // Accept: text/plain
-        System.out.println(service.path("salutation").request().accept(MediaType.TEXT_PLAIN).get().readEntity(String.class));
-        // // Get plain text
-        System.out.println(service.path("salutation")
-                .request().accept(MediaType.TEXT_PLAIN).get().readEntity(String.class));
-        // Get XML
-        System.out.println(service.path("salutation")
-                .request()
-                .accept(MediaType.TEXT_XML).get().readEntity(String.class));
-        // // The HTML
-        System.out.println(service.path("salutation").request()
-                .accept(MediaType.TEXT_HTML).get().readEntity(String.class));
-        */
+    public static RequestLog request4(String mediaType) {
+        System.out.println("Executing request4 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
+
+    public static RequestLog request5(String mediaType) {
+        System.out.println("Executing request5 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
+
+    public static RequestLog request6(String mediaType) {
+        System.out.println("Executing request6 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
+
+    public static RequestLog request7(String mediaType) {
+        System.out.println("Executing request7 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
+
+    public static RequestLog request8(String mediaType) {
+        System.out.println("Executing request8 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
+
+    public static RequestLog request9(String mediaType) {
+        System.out.println("Executing request9 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
+
+    public static RequestLog request10(String mediaType) {
+        System.out.println("Executing request10 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
+
+    public static RequestLog request11(String mediaType) {
+        System.out.println("Executing request11 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
+    }
+
+    public static RequestLog request12(String mediaType) {
+        System.out.println("Executing request12 with " + mediaType);
+        RequestLog requestLog = new RequestLog();
+        System.out.println(requestLog + "\n");
+        return requestLog;
     }
 }
---
+++ client/src/introsde/client/RequestLog.java
@@ -0,0 +1,87 @@
+package introsde.client;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+class RequestLog {
+
+    private int requestNumber;
+    private String httpMethod;
+    private String url;
+    private String acceptHeader;
+    private String contentTypeHeader;
+    private String responseStatus;
+    private int statusCode;
+    private String body;
+
+    public void log(PrintWriter out) {
+        out.println(String.format("Request #%d: %s %s Accept: %s Content-type: %s", requestNumber, httpMethod, url,
+                acceptHeader, contentTypeHeader));
+        out.println("=> Result: " + responseStatus);
+        out.println("=> HTTP STATUS: " + statusCode);
+        out.println(body);
+    }
+
+    public static String xmlToPrettyString(String xmlString) {
+        try {
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+                    .parse(new InputSource(new ByteArrayInputStream(xmlString.getBytes("utf-8"))));
+
+            // Remove whitespaces outside tags
+            NodeList nodeList = (NodeList) XPathFactory.newInstance().newXPath()
+                    .evaluate("//text()[normalize-space()='']", document, XPathConstants.NODESET);
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                node.getParentNode().removeChild(node);
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            transformerFactory.setAttribute("indent-number", 4);
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            StringWriter stringWriter = new StringWriter();
+            transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+            return stringWriter.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String jsonToPrettyString(String jsonString) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            Object json = mapper.readValue(jsonString, Object.class);
+            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
